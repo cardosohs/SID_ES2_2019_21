f2ed4dbd421550100be45803c0f5d6c48ca810f5
ricar <ricar@172.17.3.251>
2019-03-17 15:50:53 +0000

    Código Java para bulk insert

--- Monitorizacao_Culturas/src/pt/iscte/sid/projeto/Machine/JavaMysqlPreparedStatementInsertExample.java
+++ Monitorizacao_Culturas/src/pt/iscte/sid/projeto/Machine/JavaMysqlPreparedStatementInsertExample.java
@@ -18,30 +18,44 @@ public class JavaMysqlPreparedStatementInsertExample
     try
     {
       // create a mysql database connection
-      String myDriver = "org.gjt.mm.mysql.Driver";
-      String myUrl = "jdbc:mysql://localhost/test";
+      String myDriver = "com.mysql.cj.jdbc.Driver";
+      String myUrl = "jdbc:mysql://localhost/mydb?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC";
+      ////localhost:3306/database?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC
       Class.forName(myDriver);
       Connection conn = DriverManager.getConnection(myUrl, "root", "");
     
       // create a sql date object so we can use it in our INSERT statement
       Calendar calendar = Calendar.getInstance();
       java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
+      java.sql.Timestamp date = new java.sql.Timestamp(new java.util.Date().getTime());
+     
 
       // the mysql insert statement
-      String query = " insert into users (first_name, last_name, date_created, is_admin, num_points)"
-        + " values (?, ?, ?, ?, ?)";
+      String query = " insert into medicoes (IdVarMEd, ValorMed)"
+        + " values (?, ?)";
+      
+      //INSERT INTO `medicoes` (`IdVarMed`, `NumeroMed`, `DataHoraMed`, `ValorMed`) VALUES ('1', NULL, CURRENT_TIMESTAMP, '12.5');
 
       // create the mysql insert preparedstatement
       PreparedStatement preparedStmt = conn.prepareStatement(query);
-      preparedStmt.setString (1, "Barney");
-      preparedStmt.setString (2, "Rubble");
-      preparedStmt.setDate   (3, startDate);
-      preparedStmt.setBoolean(4, false);
-      preparedStmt.setInt    (5, 5000);
+      preparedStmt.setInt (1, 1);
+      //preparedStmt.setString (2, );
+      //preparedStmt.setTimestamp(3, date);
+      preparedStmt.setDouble(2, 6.77);
+      
+      long startTime = System.nanoTime();
+      System.out.println("iniciei o ciclo " + System.currentTimeMillis());
+  
 
       // execute the preparedstatement
-      preparedStmt.execute();
+      for(int i=0; i<500000; i++) {
+    	  preparedStmt.execute();    	  
+      }
       
+      long endTime = System.nanoTime();
+      System.out.println("terminei o ciclo " + System.currentTimeMillis());
+      long totalTime = endTime - startTime;
+      System.out.println("o total é " + totalTime);
       conn.close();
     }
     catch (Exception e)
