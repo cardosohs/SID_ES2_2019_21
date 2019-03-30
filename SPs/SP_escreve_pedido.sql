b23b91f18b052cc0139d446b4db97e06310d9004
vefop-iscteiul <43391233+vefop-iscteiul@users.noreply.github.com>
2019-03-30 02:33:54 +0000

    gestão de erros melhorada

--- SPs/SP_escreve_pedido.sql
+++ SPs/SP_escreve_pedido.sql
@@ -1,5 +1,14 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `escreve_pedido`(IN rtabela varchar(20), in rinicio datetime, in rfim datetime)
 BEGIN
+
+
+declare EXIT handler for SQLEXCEPTION 
+# begin
+	insert into logsg21db.log_migracao (datahora,tabela, processo, resultado) values (@horaexecucao,rtabela,'request made',0);
+# END;
+
+set @horaexecucao=now();
+
 drop table if exists pedido;
 create temporary table	pedido (
 ttabela varchar(20),
@@ -11,12 +20,11 @@ insert into pedido(ttabela,tinicio, tfim) values(rtabela,rinicio, rfim); #values
 #select * from pedido;
 
 SET @t1 =CONCAT('select * from pedido INTO OUTFILE \'c:/destino/request_',rtabela,'.csv\''); 
-#"FIELDS ENCLOSED BY '\"' 
-#TERMINATED BY ',' 
-#ESCAPED BY '\"' 
-#LINES TERMINATED BY \'\r\n\' ");
+
 PREPARE stmt3 FROM @t1;
  EXECUTE stmt3;
+ #insere sucesso para a operação de requer atualização
+ insert into logsg21db.log_migracao (datahora,tabela, processo, resultado) values (@horaexecucao,rtabela,'request made',1);
  DEALLOCATE PREPARE stmt3;
 drop table pedido;
 
