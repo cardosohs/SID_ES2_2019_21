b6018a3951c62f626abb3e4bcafd4c5d8e7bf48e
svbro-iscteiul <svbro@iscte-iul.pt>
2019-04-27 15:16:41 +0100

    TMP

--- Monitorizacao_Culturas/src/pt/iscte/sid/projeto/GUI/InserirDados.java
+++ Monitorizacao_Culturas/src/pt/iscte/sid/projeto/GUI/InserirDados.java
@@ -13,104 +13,115 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
+import pt.iscte.sid.projeto.Machine.DatabaseMiddleManForAdministrador;
+import pt.iscte.sid.projeto.Machine.DatabaseMiddleManForInvestigador;
 
 public class InserirDados {
 
-	private JFrame frmConsultarVariveis;
-	private JTable table;
-	private JButton btnAdicionarVariavel;
-	private JLabel lblSeleccionarIdDa;
-	private JTextField textField;
-	private JButton btnApagarVariavel;
-	private JLabel lblNewLabel_1;
-	private JButton btnVoltar;
-	private JLabel lblSeleccionaridDaCultura;
-	private JTextField textField_1;
+    private DatabaseMiddleManForInvestigador databaseConnection;
+    private JFrame frame;
+    private JTable table;
+    private JButton btnAdicionarVariavel;
+    private JLabel lblSeleccionarIdDa;
+    private JTextField textField;
+    private JButton btnApagarVariavel;
+    private JLabel lblNewLabel_1;
+    private JButton btnVoltar;
+    private JLabel lblSeleccionaridDaCultura;
+    private JTextField textField_1;
+    
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    InserirDados window = new InserirDados();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    
+    /**
+     * Create the application.
+     */
+    public InserirDados() {
+        initialize();
+    }
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					InserirDados window = new InserirDados();
-					window.frmConsultarVariveis.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 */
-	public InserirDados() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frmConsultarVariveis = new JFrame();
-		frmConsultarVariveis.getContentPane().setBackground(Color.WHITE);
-		frmConsultarVariveis.setTitle("Consultar Vari\u00E1veis");
-		frmConsultarVariveis.setBounds(100, 100, 850, 600);
-		frmConsultarVariveis.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmConsultarVariveis.getContentPane().setLayout(null);
-		
-		ImageIcon imagem = new ImageIcon (InserirDados.class.getResource("/images/listaVariaveis.png"));
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(110, 204, 618, 262);
-		frmConsultarVariveis.getContentPane().add(scrollPane);
-		
-		table = new JTable();
-		scrollPane.setViewportView(table);
-		
-		btnAdicionarVariavel = new JButton("Adicionar variavel");
-		btnAdicionarVariavel.setBounds(340, 511, 149, 23);
-		frmConsultarVariveis.getContentPane().add(btnAdicionarVariavel);
-		
-		lblSeleccionarIdDa = new JLabel("Seleccionar ID da Variavel");
-		lblSeleccionarIdDa.setBounds(112, 496, 149, 16);
-		frmConsultarVariveis.getContentPane().add(lblSeleccionarIdDa);
-		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(271, 490, 47, 29);
-		frmConsultarVariveis.getContentPane().add(textField);
-		
-		btnApagarVariavel = new JButton("Apagar variavel");
-		btnApagarVariavel.setBounds(499, 511, 149, 23);
-		frmConsultarVariveis.getContentPane().add(btnApagarVariavel);
-		
-		lblNewLabel_1 = new JLabel("Aqui fica a tabela Variaveis ver https://www.youtube.com/watch?v=hg1S3QHFNrE");
-		lblNewLabel_1.setBounds(209, 166, 496, 14);
-		frmConsultarVariveis.getContentPane().add(lblNewLabel_1);
-		
-		JLabel lblNewLabel = new JLabel("VER https://www.youtube.com/watch?v=hg1S3QHFNrE");
-		lblNewLabel.setBounds(0, 0, 834, 485);
-		
-		Image img = imagem.getImage().getScaledInstance(lblNewLabel.getWidth(), lblNewLabel.getHeight(), Image.SCALE_SMOOTH);
-		lblNewLabel.setIcon(new ImageIcon(img));
-		
-		frmConsultarVariveis.getContentPane().add(lblNewLabel);
-		
-		btnVoltar = new JButton("Voltar");
-		btnVoltar.setBounds(715, 526, 84, 23);
-		frmConsultarVariveis.getContentPane().add(btnVoltar);
-		
-		lblSeleccionaridDaCultura = new JLabel("ID da Cultura");
-		lblSeleccionaridDaCultura.setBounds(191, 529, 149, 16);
-		frmConsultarVariveis.getContentPane().add(lblSeleccionaridDaCultura);
-		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(271, 523, 47, 29);
-		frmConsultarVariveis.getContentPane().add(textField_1);
-	}
+    public InserirDados(DatabaseMiddleManForInvestigador databaseConnection) {
+        this.databaseConnection = databaseConnection;
+        initialize();
+    }
+    
+    private void CloseWindow() {
+        frame.setVisible(false);
+    }
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.getContentPane().setBackground(Color.WHITE);
+        frame.setTitle("Consultar Vari\u00E1veis");
+        frame.setBounds(100, 100, 850, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+        
+        ImageIcon imagem = new ImageIcon (InserirDados.class.getResource("/images/listaVariaveis.png"));
+        
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setBounds(110, 204, 618, 262);
+        frame.getContentPane().add(scrollPane);
+        
+        table = new JTable();
+        scrollPane.setViewportView(table);
+        
+        btnAdicionarVariavel = new JButton("Adicionar variavel");
+        btnAdicionarVariavel.setBounds(340, 511, 149, 23);
+        frame.getContentPane().add(btnAdicionarVariavel);
+        
+        lblSeleccionarIdDa = new JLabel("Seleccionar ID da Variavel");
+        lblSeleccionarIdDa.setBounds(112, 496, 149, 16);
+        frame.getContentPane().add(lblSeleccionarIdDa);
+        
+        textField = new JTextField();
+        textField.setColumns(10);
+        textField.setBounds(271, 490, 47, 29);
+        frame.getContentPane().add(textField);
+        
+        btnApagarVariavel = new JButton("Apagar variavel");
+        btnApagarVariavel.setBounds(499, 511, 149, 23);
+        frame.getContentPane().add(btnApagarVariavel);
+        
+        lblNewLabel_1 = new JLabel("Aqui fica a tabela Variaveis ver https://www.youtube.com/watch?v=hg1S3QHFNrE");
+        lblNewLabel_1.setBounds(209, 166, 496, 14);
+        frame.getContentPane().add(lblNewLabel_1);
+        
+        JLabel lblNewLabel = new JLabel("VER https://www.youtube.com/watch?v=hg1S3QHFNrE");
+        lblNewLabel.setBounds(0, 0, 834, 485);
+        
+        Image img = imagem.getImage().getScaledInstance(lblNewLabel.getWidth(), lblNewLabel.getHeight(), Image.SCALE_SMOOTH);
+        lblNewLabel.setIcon(new ImageIcon(img));
+        
+        frame.getContentPane().add(lblNewLabel);
+        
+        btnVoltar = new JButton("Voltar");
+        btnVoltar.setBounds(715, 526, 84, 23);
+        frame.getContentPane().add(btnVoltar);
+        
+        lblSeleccionaridDaCultura = new JLabel("ID da Cultura");
+        lblSeleccionaridDaCultura.setBounds(191, 529, 149, 16);
+        frame.getContentPane().add(lblSeleccionaridDaCultura);
+        
+        textField_1 = new JTextField();
+        textField_1.setColumns(10);
+        textField_1.setBounds(271, 523, 47, 29);
+        frame.getContentPane().add(textField_1);
+    }
 }
 
--- Monitorizacao_Culturas/src/pt/iscte/sid/projeto/GUI/ManutencaoVariaveis.java
+++ Monitorizacao_Culturas/src/pt/iscte/sid/projeto/GUI/ManutencaoVariaveis.java
@@ -15,81 +15,104 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import pt.iscte.sid.projeto.Machine.DatabaseMiddleManForAdministrador;
 
 public class ManutencaoVariaveis extends JFrame {
+    
+    private DatabaseMiddleManForAdministrador databaseConnection;
+    private JPanel contentPanel;
+    private JTextField textField;
+    private JTextField txtFaltaAdaptarPara;
+    private JFrame frame;
+    
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    ManutencaoVariaveis frame = new ManutencaoVariaveis();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField txtFaltaAdaptarPara;
+    public ManutencaoVariaveis(DatabaseMiddleManForAdministrador databaseConnection) {
+        this.databaseConnection = databaseConnection;
+        StartManutencaoVariaveis();
+    }
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ManutencaoVariaveis frame = new ManutencaoVariaveis();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public ManutencaoVariaveis() {
-		setTitle("Manuten\u00E7\u00E3o de Vari\u00E1veis");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100,  834, 530);
-		contentPane = new JPanel();
-		contentPane.setBackground(new Color(255, 255, 255));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		ImageIcon img = new ImageIcon(ManutencaoVariaveis.class.getResource("/images/manutencaoVariaveis.png"));
-		
-		textField = new JTextField();
-		textField.setBounds(165, 382, 350, 29);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
-		JButton btnAdicionarVarivel = new JButton("Adicionar Vari\u00E1vel");
-		btnAdicionarVarivel.setBounds(519, 382, 142, 29);
-		contentPane.add(btnAdicionarVarivel);
-		
-		JButton btnEliminarVarivel = new JButton("Eliminar Vari\u00E1vel");
-		btnEliminarVarivel.setBounds(663, 381, 130, 30);
-		contentPane.add(btnEliminarVarivel);
-		
-		JButton button_1 = new JButton("Voltar");
-		button_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				AreaAdmin aa = new AreaAdmin();
-				aa.setVisible(true);
-			}
-		});
-		button_1.setBounds(708, 450, 85, 30);
-		contentPane.add(button_1);
-		
-		txtFaltaAdaptarPara = new JTextField();
-		txtFaltaAdaptarPara.setText("Falta adaptar para receber a tabela Variaveis; Ver como em: https://www.youtube.com/watch?v=6cNYUc2PIag");
-		txtFaltaAdaptarPara.setColumns(10);
-		txtFaltaAdaptarPara.setBounds(220, 243, 429, 30);
-		contentPane.add(txtFaltaAdaptarPara);
-		
-		JLabel imagemFundo = new JLabel("");
-		imagemFundo.setBounds(0, 0, 818, 467);
-		
-		Image imgm = img.getImage().getScaledInstance(imagemFundo.getWidth(), imagemFundo.getHeight(), Image.SCALE_SMOOTH);
-		
-		imagemFundo.setIcon(new ImageIcon (imgm));
-		
-		contentPane.add(imagemFundo);
-	}
-
+    public ManutencaoVariaveis() {
+        StartManutencaoVariaveis();
+    }
+    
+    
+    private void CloseWindow() {
+        frame.setVisible(false);
+    }
+    
+    /**
+     * Create the frame.
+     */
+    public void StartManutencaoVariaveis() {
+        frame= new JFrame();
+        frame.setTitle("Medi\u00E7\u00F5es de Sistema");
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setBounds(100, 100, 850, 600);
+        frame.setVisible(true);
+        setTitle("Manuten\u00E7\u00E3o de Vari\u00E1veis");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100,  834, 530);
+        contentPanel = new JPanel();
+        contentPanel.setBackground(new Color(255, 255, 255));
+        contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPanel);
+        contentPanel.setLayout(null);
+        
+        ImageIcon img = new ImageIcon(ManutencaoVariaveis.class.getResource("/images/manutencaoVariaveis.png"));
+        
+        textField = new JTextField();
+        textField.setBounds(165, 382, 350, 29);
+        contentPanel.add(textField);
+        textField.setColumns(10);
+        
+        JButton btnAdicionarVarivel = new JButton("Adicionar Vari\u00E1vel");
+        btnAdicionarVarivel.setBounds(519, 382, 142, 29);
+        contentPanel.add(btnAdicionarVarivel);
+        
+        JButton btnEliminarVarivel = new JButton("Eliminar Vari\u00E1vel");
+        btnEliminarVarivel.setBounds(663, 381, 130, 30);
+        contentPanel.add(btnEliminarVarivel);
+        
+        JButton button_1 = new JButton("Voltar");
+        button_1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                AreaAdmin aa = new AreaAdmin();
+                aa.setVisible(true);
+            }
+        });
+        button_1.setBounds(708, 450, 85, 30);
+        contentPanel.add(button_1);
+        
+        txtFaltaAdaptarPara = new JTextField();
+        txtFaltaAdaptarPara.setText("Falta adaptar para receber a tabela Variaveis; Ver como em: https://www.youtube.com/watch?v=6cNYUc2PIag");
+        txtFaltaAdaptarPara.setColumns(10);
+        txtFaltaAdaptarPara.setBounds(220, 243, 429, 30);
+        contentPanel.add(txtFaltaAdaptarPara);
+        
+        JLabel imagemFundo = new JLabel("");
+        imagemFundo.setBounds(0, 0, 818, 467);
+        
+        Image imgm = img.getImage().getScaledInstance(imagemFundo.getWidth(), imagemFundo.getHeight(), Image.SCALE_SMOOTH);
+        
+        imagemFundo.setIcon(new ImageIcon (imgm));
+        
+        contentPanel.add(imagemFundo);
+        frame.add(contentPanel);
+    }
+    
 }
