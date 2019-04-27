2d8f069eb1007d9eea8e716a931d594b43f24cc2
svbro-iscteiul <svbro@iscte-iul.pt>
2019-04-27 15:16:41 +0100

    TMP

--- Monitorizacao_Culturas/src/pt/iscte/sid/projeto/GUI/AreaAdmin.java
+++ Monitorizacao_Culturas/src/pt/iscte/sid/projeto/GUI/AreaAdmin.java
@@ -16,104 +16,128 @@ import javax.swing.JButton;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import pt.iscte.sid.projeto.Machine.DatabaseMiddleManForAdministrador;
 
 public class AreaAdmin extends JFrame {
-
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application 
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					AreaAdmin frame = new AreaAdmin();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public AreaAdmin() {
-		setTitle("\u00C1rea do Administrador");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 850, 600);
-		contentPane = new JPanel();
-		contentPane.setBackground(Color.WHITE);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		ImageIcon imgTopo = new ImageIcon(SubscreverUtilizador.class.getResource("/images/areaAdmin.png"));
-		
-		JButton btnCriaoDeUtilizadores = new JButton("Cria\u00E7\u00E3o de Utilizadores");
-		btnCriaoDeUtilizadores.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				SubscreverUtilizador su = new SubscreverUtilizador();
-	//			su.setVisible(true);
-			}
-		});
-		btnCriaoDeUtilizadores.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnCriaoDeUtilizadores.setBounds(34, 368, 175, 35);
-		contentPane.add(btnCriaoDeUtilizadores);
-		
-		JButton btnManutenoDeUtilizadores = new JButton("Manuten\u00E7\u00E3o de Utilizadores");
-		btnManutenoDeUtilizadores.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				ManutencaoUtilizadores mu = new ManutencaoUtilizadores();
-				mu.setVisible(true);
-			}
-		});
-		btnManutenoDeUtilizadores.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnManutenoDeUtilizadores.setBounds(222, 368, 195, 35);
-		contentPane.add(btnManutenoDeUtilizadores);
-		
-		JButton btnManutenoDeVariveis = new JButton("Manuten\u00E7\u00E3o de Vari\u00E1veis");
-		btnManutenoDeVariveis.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				ManutencaoVariaveis mv = new ManutencaoVariaveis();
-				mv.setVisible(true);
-			}
-		});
-		btnManutenoDeVariveis.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnManutenoDeVariveis.setBounds(427, 368, 185, 35);
-		contentPane.add(btnManutenoDeVariveis);
-		
-		JButton button = new JButton("LogOut");
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				LoginWindow lw = new LoginWindow();
-				lw.setVisible(true);
-			}
-		});
-		button.setBounds(675, 499, 89, 23);
-		contentPane.add(button);
-		
-		JButton btnConfigurarLimites = new JButton("Configurar LimitesSistema");
-		btnConfigurarLimites.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				ConfigurarLimites ls = new ConfigurarLimites();
-				ls.setVisible(true);
-			}
-		});
-		btnConfigurarLimites.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnConfigurarLimites.setBounds(628, 368, 185, 35);
-		contentPane.add(btnConfigurarLimites);
-		
-		JLabel fotoTopo = new JLabel("");
-		fotoTopo.setBounds(0, 0, 834, 476);
-		Image imgOne =imgTopo.getImage().getScaledInstance(fotoTopo.getWidth(), fotoTopo.getHeight(), Image.SCALE_SMOOTH);
-		
-		fotoTopo.setIcon(new ImageIcon(imgOne));
-		
-		contentPane.add(fotoTopo);
-	}
+    
+    private JPanel contentPanel;
+    private JFrame frame;
+    private DatabaseMiddleManForAdministrador databaseConnection;
+    
+    
+    private void CloseWindow() {
+        frame.setVisible(false);
+    }
+    
+    /**
+     * Launch the application
+     */
+    public static void main(String[] args) {
+    EventQueue.invokeLater(new Runnable() {
+    public void run() {
+    try {
+    AreaAdmin frame = new AreaAdmin();
+    //frame.setVisible(true);
+    } catch (Exception e) {
+    e.printStackTrace();
+    }
+    }
+    });
+    }
+    
+    public AreaAdmin(DatabaseMiddleManForAdministrador databaseConnection) {
+        this.databaseConnection = databaseConnection;
+        StartAreaAdmin() ;
+    }
+    
+    public AreaAdmin() {
+        StartAreaAdmin() ;
+    }
+    
+    /**
+     * Create the frame.
+     */
+    public void StartAreaAdmin() {
+        frame= new JFrame();
+        frame.setTitle("\u00C1rea do Administrador");
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setBounds(100, 100, 850, 600);
+        frame.setVisible(true);
+        setTitle("\u00C1rea do Administrador");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 850, 600);
+        contentPanel = new JPanel();
+        contentPanel.setBackground(Color.WHITE);
+        contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPanel);
+        contentPanel.setLayout(null);
+        
+        ImageIcon imgTopo = new ImageIcon(SubscreverUtilizador.class.getResource("/images/areaAdmin.png"));
+        
+        JButton btnCriaoDeUtilizadores = new JButton("Cria\u00E7\u00E3o de Utilizadores");
+        btnCriaoDeUtilizadores.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                SubscreverUtilizador su = new SubscreverUtilizador(databaseConnection);
+                CloseWindow();
+            }
+        });
+        btnCriaoDeUtilizadores.setFont(new Font("Tahoma", Font.BOLD, 11));
+        btnCriaoDeUtilizadores.setBounds(34, 368, 175, 35);
+        contentPanel.add(btnCriaoDeUtilizadores);
+        
+        JButton btnManutenoDeUtilizadores = new JButton("Manuten\u00E7\u00E3o de Utilizadores");
+        btnManutenoDeUtilizadores.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                ManutencaoUtilizadores mu = new ManutencaoUtilizadores(databaseConnection);
+                CloseWindow();
+            }
+        });
+        btnManutenoDeUtilizadores.setFont(new Font("Tahoma", Font.BOLD, 11));
+        btnManutenoDeUtilizadores.setBounds(222, 368, 195, 35);
+        contentPanel.add(btnManutenoDeUtilizadores);
+        
+        JButton btnManutenoDeVariveis = new JButton("Manuten\u00E7\u00E3o de Vari\u00E1veis");
+        btnManutenoDeVariveis.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                ManutencaoVariaveis mv = new ManutencaoVariaveis(databaseConnection);
+                CloseWindow();
+            }
+        });
+        btnManutenoDeVariveis.setFont(new Font("Tahoma", Font.BOLD, 11));
+        btnManutenoDeVariveis.setBounds(427, 368, 185, 35);
+        contentPanel.add(btnManutenoDeVariveis);
+        
+        JButton button = new JButton("LogOut");
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                LoginWindow lw = new LoginWindow();
+                databaseConnection.CloseConnection();
+                CloseWindow();
+            }
+        });
+        button.setBounds(675, 499, 89, 23);
+        contentPanel.add(button);
+        
+        JButton btnConfigurarLimites = new JButton("Configurar LimitesSistema");
+        btnConfigurarLimites.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                ConfigurarLimites ls = new ConfigurarLimites();
+                CloseWindow();
+            }
+        });
+        btnConfigurarLimites.setFont(new Font("Tahoma", Font.BOLD, 11));
+        btnConfigurarLimites.setBounds(628, 368, 185, 35);
+        contentPanel.add(btnConfigurarLimites);
+        
+        JLabel fotoTopo = new JLabel("");
+        fotoTopo.setBounds(0, 0, 834, 476);
+        Image imgOne =imgTopo.getImage().getScaledInstance(fotoTopo.getWidth(), fotoTopo.getHeight(), Image.SCALE_SMOOTH);
+        
+        fotoTopo.setIcon(new ImageIcon(imgOne));
+        
+        contentPanel.add(fotoTopo);
+        frame.add(contentPanel);
+    }
 }
 
 
--- Monitorizacao_Culturas/src/pt/iscte/sid/projeto/GUI/SubscreverUtilizador.java
+++ Monitorizacao_Culturas/src/pt/iscte/sid/projeto/GUI/SubscreverUtilizador.java
@@ -22,149 +22,170 @@ import javax.swing.JSpinner;
 import javax.swing.JComboBox;
 import javax.swing.JPasswordField;
 import javax.swing.JRadioButton;
+import pt.iscte.sid.projeto.Machine.DatabaseMiddleManForAdministrador;
 
 public class SubscreverUtilizador {
+    
+    private DatabaseMiddleManForAdministrador databaseConnection;
+    private JFrame frame;
+    private JPasswordField passwordField;
+    private JPasswordField passwordField_1;
+    
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    SubscreverUtilizador window = new SubscreverUtilizador();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    
+    /**
+     * Create the application.
+     */
+    public SubscreverUtilizador() {
+        initialize();
+    }
 
-	private JFrame frmSubscreverInvestigador;
-	private JPasswordField passwordField;
-	private JPasswordField passwordField_1;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					SubscreverUtilizador window = new SubscreverUtilizador();
-					window.frmSubscreverInvestigador.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 */
-	public SubscreverUtilizador() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frmSubscreverInvestigador = new JFrame();
-		frmSubscreverInvestigador.getContentPane().setForeground(new Color(30, 144, 255));
-		frmSubscreverInvestigador.getContentPane().setBackground(Color.WHITE);
-		frmSubscreverInvestigador.setTitle("Subscrever Investigador");
-		frmSubscreverInvestigador.setBounds(100, 100, 850, 600);
-		frmSubscreverInvestigador.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmSubscreverInvestigador.getContentPane().setLayout(null);
-		
-		JLabel imagTopo = new JLabel("");
-		imagTopo.setBounds(0, 0, 834, 151);
-		
-		ImageIcon imgTopo = new ImageIcon(SubscreverUtilizador.class.getResource("/images/SubscricaoInvestigador.png"));
-		Image imgOne =imgTopo.getImage().getScaledInstance(imagTopo.getWidth(), imagTopo.getHeight(), Image.SCALE_SMOOTH);
-		
-		
-		String[] categoriasProfissionais = {" ","Phd Student", "PostDoc Student", "Investigador auxiliar", "Investigador principal", "Investigador Coordenador"};
-		JComboBox comboBox = new JComboBox(categoriasProfissionais);
-		comboBox.setToolTipText("Se Investigador");
-		comboBox.setAutoscrolls(true);
-		comboBox.setFocusTraversalPolicyProvider(true);
-		comboBox.setFocusCycleRoot(true);
-		comboBox.setDoubleBuffered(true);
-		comboBox.setEditable(true);
-		comboBox.setBounds(373, 261, 272, 22);
-		frmSubscreverInvestigador.getContentPane().add(comboBox);
-		
-		imagTopo.setIcon(new ImageIcon(imgOne));
-		
-		frmSubscreverInvestigador.getContentPane().add(imagTopo);
-		
-		JLabel lblNewLabel = new JLabel("Nome");
-		lblNewLabel.setForeground(new Color(100, 149, 237));
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel.setBounds(217, 223, 49, 27);
-		frmSubscreverInvestigador.getContentPane().add(lblNewLabel);
-		
-		JLabel lblCategoriaProfissional = new JLabel("Categoria Profissional");
-		lblCategoriaProfissional.setForeground(new Color(100, 149, 237));
-		lblCategoriaProfissional.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblCategoriaProfissional.setBounds(217, 261, 156, 27);
-		frmSubscreverInvestigador.getContentPane().add(lblCategoriaProfissional);
-		
-		JLabel lblEmail = new JLabel("E-mail");
-		lblEmail.setForeground(new Color(100, 149, 237));
-		lblEmail.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblEmail.setBounds(217, 299, 49, 27);
-		frmSubscreverInvestigador.getContentPane().add(lblEmail);
-		
-		JLabel lblPassword = new JLabel("Password");
-		lblPassword.setForeground(new Color(100, 149, 237));
-		lblPassword.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblPassword.setBounds(217, 337, 74, 27);
-		frmSubscreverInvestigador.getContentPane().add(lblPassword);
-		
-		JLabel lblConfirmarPassword = new JLabel("Confirmar Password");
-		lblConfirmarPassword.setForeground(new Color(100, 149, 237));
-		lblConfirmarPassword.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblConfirmarPassword.setBounds(217, 377, 143, 27);
-		frmSubscreverInvestigador.getContentPane().add(lblConfirmarPassword);
-		
-		TextField textField = new TextField();
-		textField.setBounds(272, 228, 373, 22);
-		frmSubscreverInvestigador.getContentPane().add(textField);
-		
-		TextField textField_3 = new TextField();
-		textField_3.setBounds(270, 304, 375, 22);
-		frmSubscreverInvestigador.getContentPane().add(textField_3);
-		
-		JButton btnOk = new JButton("Adicionar Utilizador");
-		btnOk.setForeground(new Color(25, 25, 112));
-		btnOk.setBackground(Color.WHITE);
-		btnOk.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				ManutencaoUtilizadores n= new ManutencaoUtilizadores();
-				n.setVisible(true);
-			}
-		});
-		btnOk.setBounds(612, 499, 156, 27);
-		frmSubscreverInvestigador.getContentPane().add(btnOk);
-		
-		JList list = new JList();
-		list.setBounds(484, 293, 1, 1);
-		frmSubscreverInvestigador.getContentPane().add(list);
-		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(295, 340, 350, 22);
-		frmSubscreverInvestigador.getContentPane().add(passwordField);
-		
-		passwordField_1 = new JPasswordField();
-		passwordField_1.setBounds(361, 380, 284, 22);
-		frmSubscreverInvestigador.getContentPane().add(passwordField_1);
-		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("Investigador");
-		rdbtnNewRadioButton.setFont(new Font("Tahoma", Font.BOLD, 13));
-		rdbtnNewRadioButton.setForeground(new Color(25, 25, 112));
-		rdbtnNewRadioButton.setBackground(Color.WHITE);
-		rdbtnNewRadioButton.setBounds(299, 174, 109, 23);
-		frmSubscreverInvestigador.getContentPane().add(rdbtnNewRadioButton);
-		
-		JRadioButton rdbtnAdministrador = new JRadioButton("Administrador");
-		rdbtnAdministrador.setFont(new Font("Tahoma", Font.BOLD, 13));
-		rdbtnAdministrador.setForeground(new Color(25, 25, 112));
-		rdbtnAdministrador.setBackground(Color.WHITE);
-		rdbtnAdministrador.setBounds(471, 174, 124, 23);
-		frmSubscreverInvestigador.getContentPane().add(rdbtnAdministrador);
-		
-		
-		String[] departamentos = {"Biologia", "Química"};
-	}
-
-	
+    public SubscreverUtilizador(DatabaseMiddleManForAdministrador databaseConnection) {
+        this.databaseConnection = databaseConnection;
+          initialize();
+    }
+    
+    
+    private void CloseWindow() {
+        frame.setVisible(false);
+    }
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.getContentPane().setForeground(new Color(30, 144, 255));
+        frame.getContentPane().setBackground(Color.WHITE);
+        frame.setTitle("Subscrever Investigador");
+        frame.setBounds(100, 100, 850, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+        frame.setVisible(true);
+        
+        JLabel imagTopo = new JLabel("");
+        imagTopo.setBounds(0, 0, 834, 151);
+        
+        ImageIcon imgTopo = new ImageIcon(SubscreverUtilizador.class.getResource("/images/SubscricaoInvestigador.png"));
+        Image imgOne =imgTopo.getImage().getScaledInstance(imagTopo.getWidth(), imagTopo.getHeight(), Image.SCALE_SMOOTH);
+        
+        
+        String[] categoriasProfissionais = {" ","Phd Student", "PostDoc Student", "Investigador auxiliar", "Investigador principal", "Investigador Coordenador"};
+        JComboBox comboBox = new JComboBox(categoriasProfissionais);
+        comboBox.setToolTipText("Se Investigador");
+        comboBox.setAutoscrolls(true);
+        comboBox.setFocusTraversalPolicyProvider(true);
+        comboBox.setFocusCycleRoot(true);
+        comboBox.setDoubleBuffered(true);
+        comboBox.setEditable(true);
+        comboBox.setBounds(373, 261, 272, 22);
+        frame.getContentPane().add(comboBox);
+        
+        imagTopo.setIcon(new ImageIcon(imgOne));
+        
+        frame.getContentPane().add(imagTopo);
+        
+        JLabel lblNewLabel = new JLabel("Nome");
+        lblNewLabel.setForeground(new Color(100, 149, 237));
+        lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
+        lblNewLabel.setBounds(217, 223, 49, 27);
+        frame.getContentPane().add(lblNewLabel);
+        
+        JLabel lblCategoriaProfissional = new JLabel("Categoria Profissional");
+        lblCategoriaProfissional.setForeground(new Color(100, 149, 237));
+        lblCategoriaProfissional.setFont(new Font("Tahoma", Font.BOLD, 13));
+        lblCategoriaProfissional.setBounds(217, 261, 156, 27);
+        frame.getContentPane().add(lblCategoriaProfissional);
+        
+        JLabel lblEmail = new JLabel("E-mail");
+        lblEmail.setForeground(new Color(100, 149, 237));
+        lblEmail.setFont(new Font("Tahoma", Font.BOLD, 13));
+        lblEmail.setBounds(217, 299, 49, 27);
+        frame.getContentPane().add(lblEmail);
+        
+        JLabel lblPassword = new JLabel("Password");
+        lblPassword.setForeground(new Color(100, 149, 237));
+        lblPassword.setFont(new Font("Tahoma", Font.BOLD, 13));
+        lblPassword.setBounds(217, 337, 74, 27);
+        frame.getContentPane().add(lblPassword);
+        
+        JLabel lblConfirmarPassword = new JLabel("Confirmar Password");
+        lblConfirmarPassword.setForeground(new Color(100, 149, 237));
+        lblConfirmarPassword.setFont(new Font("Tahoma", Font.BOLD, 13));
+        lblConfirmarPassword.setBounds(217, 377, 143, 27);
+        frame.getContentPane().add(lblConfirmarPassword);
+        
+        TextField textField = new TextField();
+        textField.setBounds(272, 228, 373, 22);
+        frame.getContentPane().add(textField);
+        
+        TextField textField_3 = new TextField();
+        textField_3.setBounds(270, 304, 375, 22);
+        frame.getContentPane().add(textField_3);
+        
+        JButton btnOk = new JButton("Adicionar Utilizador");
+        btnOk.setForeground(new Color(25, 25, 112));
+        btnOk.setBackground(Color.WHITE);
+        btnOk.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                ManutencaoUtilizadores n= new ManutencaoUtilizadores();
+                n.setVisible(true);
+            }
+        });
+        btnOk.setBounds(612, 499, 156, 27);
+        frame.getContentPane().add(btnOk);
+        
+        JList list = new JList();
+        list.setBounds(484, 293, 1, 1);
+        frame.getContentPane().add(list);
+        
+        passwordField = new JPasswordField();
+        passwordField.setBounds(295, 340, 350, 22);
+        frame.getContentPane().add(passwordField);
+        
+        passwordField_1 = new JPasswordField();
+        passwordField_1.setBounds(361, 380, 284, 22);
+        frame.getContentPane().add(passwordField_1);
+        
+        JRadioButton rdbtnNewRadioButton = new JRadioButton("Investigador");
+        rdbtnNewRadioButton.setFont(new Font("Tahoma", Font.BOLD, 13));
+        rdbtnNewRadioButton.setForeground(new Color(25, 25, 112));
+        rdbtnNewRadioButton.setBackground(Color.WHITE);
+        rdbtnNewRadioButton.setBounds(299, 174, 109, 23);
+        frame.getContentPane().add(rdbtnNewRadioButton);
+        
+        JRadioButton rdbtnAdministrador = new JRadioButton("Administrador");
+        rdbtnAdministrador.setFont(new Font("Tahoma", Font.BOLD, 13));
+        rdbtnAdministrador.setForeground(new Color(25, 25, 112));
+        rdbtnAdministrador.setBackground(Color.WHITE);
+        rdbtnAdministrador.setBounds(471, 174, 124, 23);
+        frame.getContentPane().add(rdbtnAdministrador);
+        
+        JButton logout = new JButton("Voltar");
+        logout.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                AreaAdmin lw = new AreaAdmin(databaseConnection);
+                CloseWindow();
+            }
+        });
+        logout.setBackground(Color.WHITE);
+        logout.setBounds(100, 499, 156, 27);
+        frame.getContentPane().add(logout);
+        String[] departamentos = {"Biologia", "Química"};
+    }
+    
+    
 }
