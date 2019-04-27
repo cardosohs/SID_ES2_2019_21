73f5576f76357e8da35d1f9fb558971b4d7f4bfd
rcfms-iscteiul <rfcms@iscte-iul.pt>
2019-04-27 15:18:07 +0100

    pom

---
+++ Monitorizacao_Culturas/src/pt/iscte/sid/projeto/sensor/Subscriber.java
@@ -0,0 +1,44 @@
+package pt.iscte.sid.projeto.sensor;
+
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class Subscriber {
+    public static void main(String[] args) {
+
+        String topic = "/sid_lab_2019";
+        String broker = "tcp://broker.mqtt-dashboard.com:1883";
+        String clientId = "Client1";
+        MemoryPersistence persistence = new MemoryPersistence();
+
+        try {
+            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            sampleClient.connect(connOpts);
+
+            sampleClient.subscribe(topic);
+            sampleClient.setCallback(new MqttCallback() {
+                public void connectionLost(Throwable throwable) {
+                    throwable.printStackTrace();
+                }
+
+                public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                    System.out.println("Topic : " + topic + " Message : " + mqttMessage);
+                }
+
+                public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+                    System.out.println("Delivery complete : " + iMqttDeliveryToken);
+                }
+            });
+            //timer para terminar recolha de info ... alterar conforme necessário
+            new CountDownLatch(1).await(100, TimeUnit.SECONDS);
+            sampleClient.disconnect();            
+        } catch (MqttException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
