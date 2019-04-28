7f326c5d9eea6bfb154d86f1145b48ab7a840d31
vefop <vefop@iscte-iul.pt>
2019-04-28 23:17:48 +0100

    Insere na BD

--- Monitorizacao_Culturas/src/migrador/MySQL.java
+++ Monitorizacao_Culturas/src/migrador/MySQL.java
@@ -1,10 +1,14 @@
 package migrador;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.bson.Document;
 
@@ -12,6 +16,10 @@ public class MySQL {
 	
 	private static String mySQLdb="g21origem"; 	
 	private static	Connection conn ;
+	private static PreparedStatement psInsereLuz;
+	private static PreparedStatement psInsereTemperatura;
+	private static PreparedStatement psInsereLog;
+	//private static PreparedStatement
 	
 static void liga() {
 		
@@ -26,23 +34,60 @@ static void liga() {
 			System.out.println("erro na ligacao ao MySQL");
 		}
 		
+		geraPreparedStatements(); 
+
 	}
 
-public static void escreveLuz(HashMap<Timestamp,Integer> Leituras) {
-//	for (Document lido : leituras) {
-//		System.out.println(lido.toJson());
-	//na tabela de luminusidade
-	//na tabela de temperatura
-	//na tabela de logs
-	//na tabela de alarmes
-//	}
+// gera as PS que podem ser usadas com esta ligacacao
+private static void geraPreparedStatements() {
+	String queryInsereLuz = "insert into medicoesluz (DataHoraMed,ValorMedicaoLuz)" + " values (?,?)";
+	String queryInsereTemperatura = "insert into medicoestemp (DataHoraMed,ValorMedicaoTemp)" + " values (?,?)";
+	String queryInsereLog="insert into sensormigracaolog (momento,ordem)" + " values (?,?)";
 	
+	//as PreparedStatement  são variaveis globais, declaradas no inicio da classe
+	try {
+		psInsereLuz=conn.prepareStatement(queryInsereLuz);
+		psInsereTemperatura=conn.prepareStatement(queryInsereTemperatura);
+		psInsereLog=conn.prepareStatement(queryInsereLog);
+		
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		System.out.println("Erro ao criar prepared Statement");
+	}
+	
+
+	
+}
+
+//insere dados na tabela de Mysql de leituras de Luminusidade
+public static void escreveLuz(HashMap<Timestamp,Integer> Leituras) {
+ for (Map.Entry me : Leituras.entrySet()) {
+	try {
+		psInsereLuz.setTimestamp(1, (Timestamp) me.getKey());
+		psInsereLuz.setInt(2, (int) me.getValue());
+		psInsereLuz.execute(); 
+		
+	} catch (SQLException e) {
+		System.out.println("Erro na PS insere Luz");
+		System.out.println(e.toString());
+	}
+	
+ }
 	
 }
 
 public static void escreveTemperatura(HashMap<Timestamp,Double> Leituras) {
-	//
-	
+	 for (Map.Entry me : Leituras.entrySet()) {
+			try {
+				psInsereTemperatura.setTimestamp(1, (Timestamp) me.getKey());
+				psInsereTemperatura.setDouble(2, (Double) me.getValue());
+				psInsereTemperatura.execute(); 
+				
+			} catch (SQLException e) {
+				System.out.println("Erro na PS insere Temperatura");
+			}
+			
+		 }
 }
 
 
