68d679883aeb4563a2a8281e37a16acc97797567
hcsao1-iscteiul <hcsao1@iscte-iul.pt>
2019-04-30 12:30:32 +0100

    Updated: method createDocuments

--- Monitorizacao_Culturas/src/pt/iscte/sid/projeto/sensor/Subscriber.java
+++ Monitorizacao_Culturas/src/pt/iscte/sid/projeto/sensor/Subscriber.java
@@ -5,10 +5,15 @@ import org.bson.Document;
 import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
@@ -17,27 +22,8 @@ import java.util.concurrent.TimeUnit;
 public class Subscriber {
 	
 	
-	private static String [] resultados  = new String [3];
+	//private static String [] resultados  = new String [3];
 	private static List<Document> docs = new ArrayList<Document>();
-	
-	
-	/**
-	 * Create two documents and add then into a array 
-	 */
-	private static void createDocuments () {
-		if (!resultados[1].equals("NULL")) {
-			Document docTemp = new Document ("tmp", resultados[1])
-							.append("timestamp", resultados[0])
-							.append("time_med", new BsonTimestamp());
-			docs.add(docTemp);
-		}
-		if (!resultados[2].equals("NULL")) {
-			Document docLuz = new Document ("lum", resultados[2]).
-							append("timestamp", resultados[0]).
-							append("time_med", new BsonTimestamp());
-			docs.add(docLuz);
-		}
-	}
 	
 	
 	public static void main(String[] args) {
@@ -49,16 +35,15 @@ public class Subscriber {
 		String colName = "MedicoesSensor";
 		 
 		MemoryPersistence persistence = new MemoryPersistence();
-
 		
-		//Estabelece ligaÃ§Ã£o com a MongoDB PRIMARIA
+		//Estabelece ligacao com a MongoDB PRIMARIA
 		MongoClient mongoClient = new MongoClient("localhost", 27017);
 		System.out.println("Connection established");
 
 		//Request da DB
 		MongoDatabase database = mongoClient.getDatabase(dbName);
 
-		//Request da coleÃ§Ã£o
+		//Request da colecao
 		MongoCollection<Document> collection = database.getCollection(colName);
 		 
 
@@ -77,18 +62,20 @@ public class Subscriber {
 				
 				// Sample [28/4/2019 11:10:43, 32.20, 5]
 				public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
-					String js1 = mqttMessage.toString();
-					resultados = MessageParser.parse(js1);
+					//String js1 = mqttMessage.toString();
+					//resultados = MessageParser.parse(js1);
 					
-					createDocuments();
-					collection.insertMany(docs);
+					createDocuments(mqttMessage);
+					
+					if (!docs.isEmpty())
+						for (Document d : docs)
+							collection.insertOne(d);
+					
 					docs.clear();
 					
 					System.out.println("Topic : " + topic + " Message : " + mqttMessage);
 
-					System.out.println(resultados);
-
-
+					//System.out.println(resultados);
 				}
 
 				public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
@@ -103,5 +90,55 @@ public class Subscriber {
 			e.printStackTrace();
 		}
 		mongoClient.close();
+	}
+	
+	
+	/**
+	 * Create two documents and add then into a array 
+	 */
+	private static void createDocuments (MqttMessage mqttMessage) {
+		
+		String message = mqttMessage.toString();
+		
+		//Insere a vírgula que faltava na mensagem recebida
+		String treated = message.replace("\"\"", "\",\"");
+		
+		//Strings inicializadas com "NULL" para posterior tratamento
+		String data="NULL";
+		String hora="NULL";
+		String temp="NULL";
+		String lum="NULL";
+		
+		//Objetos necessários para a identificação e atribuição de 
+		//jsonNodes as strings da mensagem recebida
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode jsonNode;
+		try {
+			jsonNode = objectMapper.readTree(treated);
+			data = jsonNode.get("dat").asText();
+			hora = jsonNode.get("tim").asText();
+			temp = jsonNode.get("tmp").asText();
+			lum = jsonNode.get("cell").asText();
+		} catch (JsonProcessingException e) {
+			System.out.println("erro ao processar mensagem Valor colocado a NULL");
+		} catch (IOException e) {
+			System.out.println("erro de IO");
+			e.printStackTrace();
+		}
+		
+		//Se a mensagem não possui valores de luminosidade ou temperatura
+		//não é criado o documento respectivo
+		if (!temp.equals("NULL")) {
+			Document docTemp = new Document ("tmp", temp)
+							.append("timestamp", data + " " + hora)
+							.append("time_med", new BsonTimestamp());
+			docs.add(docTemp);
+		}
+		if (!lum.equals("NULL")) {
+			Document docLuz = new Document ("lum", lum).
+							append("timestamp", data + " " + hora).
+							append("time_med", new BsonTimestamp());
+			docs.add(docLuz);
+		}
 	}
 }
