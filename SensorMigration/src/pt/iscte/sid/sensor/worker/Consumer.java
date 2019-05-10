305f7c25ecaf10699740c64bfc7b6bf996131fa5
rcfms-iscteiul <rfcms@iscte-iul.pt>
2019-05-10 13:33:00 +0100

    Clean Slate

--- SensorMigration/src/pt/iscte/sid/sensor/worker/Consumer.java
+++ SensorMigration/src/pt/iscte/sid/sensor/worker/Consumer.java
@@ -1,19 +1,2 @@
 package pt.iscte.sid.sensor.worker;
 
-import java.util.concurrent.BlockingQueue;
-
-public class Consumer implements Runnable  {
-	
-	protected BlockingQueue<Object> queue;
-	
-	public Consumer(BlockingQueue<Object> theQueue) {
-        this.queue = theQueue;
-    }
-
-	@Override
-	public void run() {
-		
-		
-	}
-
-}
