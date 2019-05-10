805269c47ababc1fc75fde007caac5c8c2cf612a
vefop <vefop@iscte-iul.pt>
2019-05-10 17:37:29 +0100

    Com ciclo While (True)

--- Monitorizacao_Culturas/src/pt/iscte/sid/projeto/migrador/Migrador.java
+++ Monitorizacao_Culturas/src/pt/iscte/sid/projeto/migrador/Migrador.java
@@ -15,47 +15,45 @@ public class Migrador {
 //	private final static String tabelaTemperatura="medicoestemp";
 
 	public static void main(String[] args) {
-		// inicializacao de variaveis
-//		List<Document> leiturasLuz = new ArrayList<Document>();
-//		List<Document> leiturasTemperatura = new ArrayList<Document>();
-		List<Document> leituras = new ArrayList<Document>();
+		while (true) {
+			// inicializacao de variaveis
+			List<Document> leituras = new ArrayList<Document>();
 
-		// estabelcer ligações
-		MySQL.liga();
-		MongoDb.liga();
+			// estabelcer ligações
+			MySQL.liga();
+			MongoDb.liga();
 
-		// tenta obter o último log e outros dados de inicializacao
-		DadosInicializacao init=null;
-		try {
-			init = Inicializa.getInitData();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			// tenta obter o último log e outros dados de inicializacao
+			DadosInicializacao init = null;
+			try {
+				init = Inicializa.getInitData();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			// ler dados MongoDb
+			if (init != null) {
+				// ou pede os dados depois do último log
+				leituras = MongoDb.le(init.ultimoLog);
+
+			} else {
+				// ou pede todos os dados
+				leituras = MongoDb.le();
+			}
+			// prepara os dados
+			ConcurrentHashMap<BsonTimestamp, Medicao> leiturasTodas = Processa.prepara(leituras);
+
+			// escrever dados no MySQL
+			MySQL.escreveSensorMigracaoLog(leiturasTodas);
+
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				System.out.println("Acordou do Slepp");
+				e.printStackTrace();
+			}
+
 		}
-
-		// ler dados MongoDb
-		if (init != null) {
-			// ou pede os dados depois do último log
-//			leiturasLuz = MongoDb.le("lum", init.ultimoLog);
-//			leiturasTemperatura = MongoDb.le("tmp",init.ultimoLog);
-			leituras = MongoDb.le(init.ultimoLog);
-
-		} else {
-			// ou pede todos os dados
-//			leiturasLuz = MongoDb.le("lum");
-//			leiturasTemperatura = MongoDb.le("tmp");
-			leituras = MongoDb.le();
-		}
-		// prepara os dados
-//		ConcurrentHashMap<Timestamp, MedicaoLuz> leiturasLuzProntas = Processa.preparaLuz(leiturasLuz);
-//		ConcurrentHashMap<Timestamp, MedicaoTemperatura> leiturasTemperaturaProntas = Processa.preparaTemperatura(leiturasTemperatura);
-		ConcurrentHashMap<BsonTimestamp, Medicao> leiturasTodas = Processa.prepara(leituras);
-		
-		// escrever dados no MySQL
-//		MySQL.escreveLuz(leiturasLuzProntas);
-//		MySQL.escreveTemperatura(leiturasTemperaturaProntas);
-		MySQL.escreveSensorMigracaoLog(leiturasTodas);
-		
 	}
-
 }
