9836c54278c2f2d89fef1bca73cb1376bb9f9fe0
AlexandreSilva <AlexandreSilva@192.168.1.5>
2019-05-11 15:50:38 +0100

    javadoc da classe areainvestigador

--- Monitorizacao_Culturas/src/pt/iscte/sid/projeto/GUI/AreaInvestigador.java
+++ Monitorizacao_Culturas/src/pt/iscte/sid/projeto/GUI/AreaInvestigador.java
@@ -21,6 +21,13 @@ import java.awt.event.ActionEvent;
 import java.awt.SystemColor;
 import pt.iscte.sid.projeto.Machine.DatabaseMiddleManForInvestigador;
 
+/**
+ * 
+ * @author Grupo 21
+ * Esta classe e referente a interface grafico para o Investigador
+ *
+ */
+
 public class AreaInvestigador extends JFrame {
     
     private JPanel contentPanel;
@@ -31,9 +38,7 @@ public class AreaInvestigador extends JFrame {
     
     
     
-    /**
-     * Launch the application.
-     */
+    
   /*  public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -50,6 +55,12 @@ public class AreaInvestigador extends JFrame {
         frame.setVisible(false);
     }
     
+    
+    /**
+     * Lanca a interface do Investigador
+     * 
+     * @param databaseConnection  esta parametro é a ligacao da base de dados
+     */
     public AreaInvestigador(DatabaseMiddleManForInvestigador databaseConnection) {
         this.databaseConnection = databaseConnection;
         StartAreaInvestigador();
@@ -57,7 +68,7 @@ public class AreaInvestigador extends JFrame {
 
     
     /**
-     * Create the frame.
+     * Cria a janela do Investigador.
      */
     public void StartAreaInvestigador() {
         frame= new JFrame();
