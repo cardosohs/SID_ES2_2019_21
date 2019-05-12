a73f0f46a356b08ac565e834bf647deb9ccaec0d
rcfms-iscteiul <rfcms@iscte-iul.pt>
2019-05-12 15:14:11 +0100

    test consultarluztemp

--- Monitorizacao_Culturas/test/pt/iscte/sid/projeto/GUI/test/ConfigurarLimitesTest.java
+++ Monitorizacao_Culturas/test/pt/iscte/sid/projeto/GUI/test/ConfigurarLimitesTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import pt.iscte.sid.projeto.GUI.AreaAdmin;
+
 import pt.iscte.sid.projeto.GUI.ConfigurarLimites;
 import pt.iscte.sid.projeto.Machine.DatabaseMiddleManForAdministrador;
 
