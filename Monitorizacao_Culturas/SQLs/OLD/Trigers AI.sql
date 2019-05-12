caaaa41aec439a9529405a13a1c7168134ea34c3
vefop <vefop@iscte-iul.pt>
2019-05-12 14:23:59 +0100

    testes

---
+++ Monitorizacao_Culturas/SQLs/OLD/Trigers AI.sql
@@ -0,0 +1,67 @@
+-- ao receber uma medicao nova em sensormigracaolog vai chamar a SP respectiva para identificar se é outlier ou nao
+
+CREATE DEFINER=`root`@`localhost` TRIGGER `g21origem`.`processa`
+ AFTER INSERT ON `sensormigracaolog` FOR EACH ROW
+BEGIN
+CASE
+	WHEN new.sensor="lum" THEN CALL sp_processaLuz(new.momento,new.ordem, new.sensor, new.medicao, new.datetime_sensor);
+   WHEN new.sensor='tmp' then CALL sp_processaTemp(new.momento,new.ordem, new.sensor, new.medicao, new.datetime_sensor);
+end case;
+END
+
+-- fim
+-- ---
+
+
+
+
+
+-- verifica medicoes de luz
+
+CREATE DEFINER=`root`@`localhost` TRIGGER `g21origem`.`medicoestemp_AFTER_INSERT` AFTER INSERT ON `medicoestemp` FOR EACH ROW
+BEGIN
+	DECLARE LimiteInf Double;
+	DECLARE LimiteSup Double;
+-- obtem os valores da tabela sistema
+set limiteinf = (select limiteinferiorTemp from sistema limit 1);
+set limitesup = (select limiteSuperiorTemp from sistema limit 1);
+-- verifica se o novo valor ultrapassa algum
+
+	if( new.ValorMedicaoTemp > limitesup ) then 
+		  insert into alertas (DataHora,NomeVariavel, LimiteInferior, LimiteSuperior, ValorMedicao,Descricao)
+			values (new.DataHoraMed,"Temperatura",LimiteInf,LimiteSup, new.ValorMedicaoTemp,"Acima do limite");
+	end if;
+    
+	if( new.ValorMedicaoTemp < limiteinf ) then 
+      insert into alertas (DataHora,NomeVariavel, LimiteInferior, LimiteSuperior, ValorMedicao,Descricao)
+		values (new.DataHoraMed,"Temperatura",LimiteInf,LimiteSup, new.ValorMedicaoTemp,"Abaixo do limite");
+	end if;
+END
+
+-- -- fim
+
+-- -- verifica alarmes nas medicoes de temperatura
+CREATE DEFINER=`root`@`localhost` TRIGGER `g21origem`.`medicoestemp_AFTER_INSERT` AFTER INSERT ON `medicoestemp` FOR EACH ROW
+BEGIN
+	DECLARE LimiteInf Double;
+	DECLARE LimiteSup Double;
+-- obtem os valores da tabela sistema
+set limiteinf = (select limiteinferiorTemp from sistema limit 1);
+set limitesup = (select limiteSuperiorTemp from sistema limit 1);
+-- verifica se o novo valor ultrapassa algum
+
+	if( new.ValorMedicaoTemp > limitesup ) then 
+		  insert into alertas (DataHora,NomeVariavel, LimiteInferior, LimiteSuperior, ValorMedicao,Descricao)
+			values (new.DataHoraMed,"Temperatura",LimiteInf,LimiteSup, new.ValorMedicaoTemp,"Acima do limite");
+	end if;
+    
+	if( new.ValorMedicaoTemp < limiteinf ) then 
+      insert into alertas (DataHora,NomeVariavel, LimiteInferior, LimiteSuperior, ValorMedicao,Descricao)
+		values (new.DataHoraMed,"Temperatura",LimiteInf,LimiteSup, new.ValorMedicaoTemp,"Abaixo do limite");
+	end if;
+	
+
+
+END
+
+-- fim  
