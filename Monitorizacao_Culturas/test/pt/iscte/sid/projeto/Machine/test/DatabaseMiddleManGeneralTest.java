32256b1747531702db733f02c52e678bca0beeff
vefop <vefop@iscte-iul.pt>
2019-05-12 17:11:17 +0100

    Junit test

--- Monitorizacao_Culturas/test/pt/iscte/sid/projeto/Machine/test/DatabaseMiddleManGeneralTest.java
+++ Monitorizacao_Culturas/test/pt/iscte/sid/projeto/Machine/test/DatabaseMiddleManGeneralTest.java
@@ -4,41 +4,97 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import pt.iscte.sid.projeto.Machine.DatabaseMiddleManGeneral;
+import pt.iscte.sid.projeto.Machine.UserIdentifier;
+
 class DatabaseMiddleManGeneralTest {
+
+	DatabaseMiddleManGeneral goodDatabase;
+	DatabaseMiddleManGeneral badDatabase;
+	String goodUsername = "vasco";
+	String goodPassword = "vasco";
+	String badUsername = "carlos";
+	String badPassword = "312";
 
 	@Test
 	final void testDatabaseMiddleManGeneral() {
-		fail("Not yet implemented"); // TODO
-	}
 
-	@Test
-	final void testCloseConnection() {
-		fail("Not yet implemented"); // TODO
+		// NotOK
+		badDatabase = new DatabaseMiddleManGeneral(badUsername, goodPassword);
+		assertNotNull(badDatabase);
+
+		badDatabase = new DatabaseMiddleManGeneral(badUsername, badPassword);
+		assertNotNull(badDatabase);
+
+		// OK
+		goodDatabase = new DatabaseMiddleManGeneral(goodUsername, goodPassword);
+		assertNotNull(goodDatabase);
+
 	}
 
 	@Test
 	final void testFailed() {
-		fail("Not yet implemented"); // TODO
+		Boolean ok = true;
+		Boolean ko = true;
+
+		// assertEquals(ko, goodDatabase.failed());
+
+		assertThrows(NullPointerException.class, () -> {
+			assertEquals(ok, badDatabase.failed());
+
+		});
+
 	}
 
 	@Test
 	final void testGetMyId() {
-		fail("Not yet implemented"); // TODO
+		//assertNotNull(goodDatabase.getMyId());
+		assertThrows(NullPointerException.class, () -> {
+			assertNotNull(badDatabase.getMyId());
+
+		});
+
 	}
 
 	@Test
 	final void testGetMyName() {
-		fail("Not yet implemented"); // TODO
+		// assertNotNull(goodDatabase.getMyName());
+		assertThrows(NullPointerException.class, () -> {
+			assertNotNull(badDatabase.getMyName());
+		});
+
 	}
 
 	@Test
 	final void testGetVariaveis() {
-		fail("Not yet implemented"); // TODO
+		//assertNotNull(goodDatabase.getVariaveis());
+		assertThrows(NullPointerException.class, () -> {
+			assertNotNull(badDatabase.getVariaveis());
+		});
 	}
 
 	@Test
 	final void testGetSistema() {
-		fail("Not yet implemented"); // TODO
+		// assertNotNull(goodDatabase.getVariaveis());
+		assertThrows(NullPointerException.class, () -> {
+			assertNotNull(badDatabase.getVariaveis());
+		});
+
+	}
+
+	@Test
+	final void testCloseConnection() {
+		Boolean ok = true;
+		Boolean ko = true;
+
+//		assertEquals(ok, goodDatabase.closeConnection());
+		assertThrows(NullPointerException.class, () -> {
+			assertEquals(ko, badDatabase.closeConnection());
+
+		});
+
+
+
 	}
 
 }
--- Monitorizacao_Culturas/test/pt/iscte/sid/projeto/Machine/test/UserIdentifierTest.java
+++ Monitorizacao_Culturas/test/pt/iscte/sid/projeto/Machine/test/UserIdentifierTest.java
@@ -2,13 +2,50 @@ package pt.iscte.sid.projeto.Machine.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
+
+import pt.iscte.sid.projeto.GUI.ConfigurarLimites;
+import pt.iscte.sid.projeto.Machine.UserIdentifier;
 
 class UserIdentifierTest {
 
+ final String DatabaseName="g21origem";
+ UserIdentifier user;
+     String goodUsername="vasco";
+     String goodPassword="vasco";
+     String badUsername="carlos";
+     String badPassword="312";
+     
+    
+    
+	
+	
+	
 	@Test
 	final void testWhatUserIsThis() {
-		fail("Not yet implemented"); // TODO
+		String ok = "A";
+		String notOK = "F";
+		
+		assertThrows(NullPointerException.class,
+	            ()->{
+	            	assertSame(ok, user.whatUserIsThis(goodUsername, goodPassword));
+	            	});
+		
+		assertThrows(NullPointerException.class,
+	            ()->{
+	            	assertSame(notOK, user.whatUserIsThis(goodUsername, badPassword));
+	            	});
+		
+		assertThrows(NullPointerException.class,
+	            ()->{
+	            	assertNotNull( user.whatUserIsThis(badUsername, badPassword));
+	            	});
+		
+		
+		
 	}
 
 }
