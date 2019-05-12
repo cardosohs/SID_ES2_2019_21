629105c9dd9b21931dffa430f1a90cdc81b65e8a
rcfms-iscteiul <rfcms@iscte-iul.pt>
2019-05-12 15:56:43 +0100

    manutencaoutilizador junit

--- Monitorizacao_Culturas/test/pt/iscte/sid/projeto/GUI/test/ManutencaoUtilizadoresTest.java
+++ Monitorizacao_Culturas/test/pt/iscte/sid/projeto/GUI/test/ManutencaoUtilizadoresTest.java
@@ -4,16 +4,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import pt.iscte.sid.projeto.GUI.CulturasLista;
+import pt.iscte.sid.projeto.GUI.ManutencaoUtilizadores;
+import pt.iscte.sid.projeto.Machine.DatabaseMiddleManForAdministrador;
+
 class ManutencaoUtilizadoresTest {
+	
+	
+	ManutencaoUtilizadores mu;
+	private DatabaseMiddleManForAdministrador databaseConnection;
 
 	@Test
 	final void testManutencaoUtilizadores() {
-		fail("Not yet implemented"); // TODO
+		assertThrows(NullPointerException.class,
+	            ()->{
+	            	mu = new ManutencaoUtilizadores(databaseConnection);
+	            	});
 	}
 
-	@Test
-	final void testStartManutencaoUtilizadores() {
-		fail("Not yet implemented"); // TODO
-	}
+
 
 }
